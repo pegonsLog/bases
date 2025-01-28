8ead2fecfb9359750e90c8ef2cd878c1705e530a
Pedro Gonçalves Sobrinho <pegons.log@gmail.com>
2025-01-28 09:35:29 -0300

    28/01/2025 - Trabalho

---
+++ src/app/auth/pages/completar-registro/completar-registro.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-completar-registro',
+  templateUrl: './completar-registro.component.html',
+  styleUrls: ['./completar-registro.component.scss'],
+})
+export class CompletarRegistroComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/auth/pages/request-login/request-login.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-login',
+  templateUrl: './request-login.component.html',
+  styleUrls: ['./request-login.component.scss'],
+})
+export class RequestLoginComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
